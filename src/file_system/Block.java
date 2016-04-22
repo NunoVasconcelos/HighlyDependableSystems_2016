43d54943942d2a99dae5cb5a237677a4c4e8626f
Nuno Vasconcelos <nunorogerio12@hotmail.com>
2016-04-22 14:57:53 +0100

    TODO added getHigherTimeStamp()

--- src/file_system/Block.java
+++ src/file_system/Block.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 abstract public class Block {
     protected int timestamp;
 
+    //TODO we need to implement the getHigherTimestamp() in blockserver (on put_k and put_h)
+
     public int getTimestamp() { return timestamp; }
     public void setTimestamp(int timestamp)
     {this.timestamp = timestamp;}
